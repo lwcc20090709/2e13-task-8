f610bf38a2220636bd5cc30853e28ca88916e13c
lwcc20090709 <115065039+lwcc20090709@users.noreply.github.com>
2022-11-09 14:51:41 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,3 +18,30 @@ input.onButtonPressed(Button.A, function () {
         music.playMelody("B A G A G F A C5 ", 45)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    degree = input.compassHeading()
+    if (degree >= 0) {
+        basic.showString("N")
+    }
+    if (degree == 45) {
+        basic.showString("NE")
+    }
+    if (degree > 90) {
+        basic.showString("E")
+    }
+    if (degree == 135) {
+        basic.showString("SE")
+    }
+    if (degree > 180) {
+        basic.showString("S")
+    }
+    if (degree == 225) {
+        basic.showString("SW")
+    }
+    if (degree > 270) {
+        basic.showString("W")
+    }
+    if (degree == 315) {
+        basic.showString("NW")
+    }
+})
